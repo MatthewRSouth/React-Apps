f14d31735e1762f4a042df8726870e8c4d2aeeb0
MatthewRSouth <matthewrogersouth@gmail.com>
2025-08-22 20:17:13 +0900

    Finished up the features and restyled

--- core-concepts-practice/src/App.jsx
+++ core-concepts-practice/src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
-import './components/ProfileCard.jsx';
 import ProfileCard from './components/ProfileCard.jsx';
 
 function App() {
     const [name, setName] = useState('Your Name');
     const [bio, setBio] = useState('Please write about yourself...');
-    const [color, setColor] = useState('What is your favorite color?');
+    const [color, setColor] = useState('#0000');
+    const [font, setFont] = useState('Arial');
+
+    // function getColor()
 
     return (
         <>
@@ -30,11 +30,39 @@ function App() {
                         autoCorrect="on"
                         onChange={(e) => setBio(e.target.value)}
                     ></textarea>
-                    <input type="color" className="card-item" />
+                    <label htmlFor="favorite-color">
+                        What is your favorite color?
+                    </label>
+                    <br />
+                    <input
+                        type="color"
+                        id="favorite-color"
+                        className="card-item color-picker"
+                        value={color}
+                        onChange={(e) => setColor(e.target.value)}
+                    />{' '}
+                    <br />
+                    <label htmlFor="fonts">
+                        What font would you like the card to use?
+                    </label>
+                    <br />
+                    <select
+                        name="fonts"
+                        id="fonts"
+                        onChange={(e) => setFont(e.target.value)}
+                    >
+                        <option value="Arial">Arial</option>
+                        <option value="Georgia">Georigia</option>
+                        <option value="Verdana">Verdana</option>
+                    </select>
                 </div>
-                <div className="card card-item">
-                    <ProfileCard name={name}>name</ProfileCard>
-                </div>
+
+                <ProfileCard
+                    name={name}
+                    bio={bio}
+                    favoriteColor={color}
+                    font={font}
+                ></ProfileCard>
             </div>
         </>
     );
--- core-concepts-practice/src/components/ProfileCard.jsx
+++ core-concepts-practice/src/components/ProfileCard.jsx
@@ -1,7 +1,35 @@
 import '../App.css';
 import '../App';
 
-export default function ProfileCard({ children, name, bio, favoriteColor }) {
-    <h1>{name}</h1>;
-    <h4>{bio}</h4>;
+// .card {
+//     flex-direction: column;
+//     flex-wrap: wrap;
+//     height: 40rem;
+//     width: 28rem;
+//     border: 2px solid black;
+//     border-radius: 10px;
+//     text-align: center;
+// }
+
+export default function ProfileCard({ name, bio, favoriteColor, font }) {
+    const cardStyle = {
+        borderColor: favoriteColor,
+        borderWidth: '2px',
+        borderStyle: 'solid',
+        borderRadius: '10px',
+        height: '40rem',
+        width: '28rem',
+        textAlign: 'center',
+        flexDirection: 'column',
+        flexWrap: 'wrap',
+        marginTop: '1.1rem',
+        fontFamily: font,
+    };
+
+    return (
+        <div style={cardStyle}>
+            <h1>{name}</h1>
+            <h4>{bio}</h4>
+        </div>
+    );
 }
