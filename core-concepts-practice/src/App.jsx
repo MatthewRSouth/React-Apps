5bac2ef60fa5ae0b18467d37bf2bfc940556e99f
MatthewRSouth <matthewrogersouth@gmail.com>
2025-08-22 09:04:25 +0900

    created the cards and styled them

--- core-concepts-practice/src/App.jsx
+++ core-concepts-practice/src/App.jsx
@@ -2,23 +2,38 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
+import './components/ProfileCard.jsx';
+import ProfileCard from './components/ProfileCard.jsx';
 
 function App() {
     const [name, setName] = useState('Your Name');
     const [bio, setBio] = useState('Please write about yourself...');
+    const [color, setColor] = useState('What is your favorite color?');
 
     return (
         <>
             <div className="cardContainer">
-                <div className="card">
-                    <input type="text" value={name} />
+                <div className="card card-item">
+                    <input
+                        type="text"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                    />
                     <textarea
+                        type="text"
                         name="Bio"
                         id="bio"
                         value={bio}
                         cols="50"
                         rows="20"
+                        className="card-item"
+                        autoCorrect="on"
+                        onChange={(e) => setBio(e.target.value)}
                     ></textarea>
+                    <input type="color" className="card-item" />
+                </div>
+                <div className="card card-item">
+                    <ProfileCard name={name}>name</ProfileCard>
                 </div>
             </div>
         </>
---
+++ core-concepts-practice/src/components/ProfileCard.jsx
@@ -0,0 +1,7 @@
+import '../App.css';
+import '../App';
+
+export default function ProfileCard({ children, name, bio, favoriteColor }) {
+    <h1>{name}</h1>;
+    <h4>{bio}</h4>;
+}
